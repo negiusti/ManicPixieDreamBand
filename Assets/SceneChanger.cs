95085c2728bd3f42659b5cbbc4f9e81a90b1a87e
Nicole Giusti <nicoleegiusti@gmail.com>
2023-10-06 14:45:10 -0700

    Don't need to unload previous scene, just end conversations before switching scenes

--- Assets/SceneChanger.cs
+++ Assets/SceneChanger.cs
@@ -2,23 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using PixelCrushers.DialogueSystem;
 
 public class SceneChanger : MonoBehaviour
 {
     //private GameObject player;
     public void ChangeScene(string sceneName)
     {
+        DialogueManager.StopAllConversations();
         string currentScene = SceneManager.GetActiveScene().name;
         Debug.Log("Loading scene: " + sceneName);
-        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneName);
-        loadOperation.completed += (operation) => OnLoadCompleted(operation, currentScene);
+        //AsyncOperation loadOperation =
+        SceneManager.LoadSceneAsync(sceneName);
+        //loadOperation.completed += (operation) => OnLoadCompleted(operation, currentScene);
     }
 
-    private void OnLoadCompleted(AsyncOperation operation, string scene)
-    {
-        Debug.Log("Unloading scene: " + scene);
-        SceneManager.UnloadSceneAsync(scene);
-    }
+    //private void OnLoadCompleted(AsyncOperation operation, string scene)
+    //{
+    //    Debug.Log("Unloading scene: " + scene);
+    //    SceneManager.UnloadSceneAsync(scene);
+    //}
 
     // Start is called before the first frame update
     void Start()
