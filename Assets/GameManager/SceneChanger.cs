ad642ac7e9534dc9c4f62bd06106d85447477fb3
Nicole Giusti <nicoleegiusti@gmail.com>
2023-10-11 18:15:54 -0700

    Use custom lua scripts in dialogue system

--- Assets/GameManager/SceneChanger.cs
+++ Assets/GameManager/SceneChanger.cs
@@ -10,18 +10,34 @@ public class SceneChanger : MonoBehaviour
     public void ChangeScene(string sceneName)
     {
         DialogueManager.StopAllConversations();
-        string currentScene = SceneManager.GetActiveScene().name;
+        //string currentScene = SceneManager.GetActiveScene().name;
         Debug.Log("Loading scene: " + sceneName);
         //AsyncOperation loadOperation =
         SceneManager.LoadSceneAsync(sceneName);
         //loadOperation.completed += (operation) => OnLoadCompleted(operation, currentScene);
     }
 
-    //private void OnLoadCompleted(AsyncOperation operation, string scene)
-    //{
-    //    Debug.Log("Unloading scene: " + scene);
-    //    SceneManager.UnloadSceneAsync(scene);
-    //}
+    void OnEnable()
+    {
+        // Make the functions available to Lua: (Replace these lines with your own.)
+        Lua.RegisterFunction(nameof(DebugLog), this, SymbolExtensions.GetMethodInfo(() => DebugLog(string.Empty)));
+        Lua.RegisterFunction(nameof(ChangeScene), this, SymbolExtensions.GetMethodInfo(() => ChangeScene(string.Empty)));
+    }
+
+    void OnDisable()
+    {
+        //if (unregisterOnDisable)
+        //{
+            // Remove the functions from Lua: (Replace these lines with your own.)
+            Lua.UnregisterFunction(nameof(DebugLog));
+            Lua.UnregisterFunction(nameof(ChangeScene));
+        //}
+    }
+
+    public void DebugLog(string message)
+    {
+        Debug.Log(message);
+    }
 
     // Start is called before the first frame update
     void Start()
