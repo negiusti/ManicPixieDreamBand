09b0b13d5a7a1727f0fb863e5a3bdda97ab0e635
Nicole Giusti <nicoleegiusti@gmail.com>
2023-10-30 14:48:49 -0700

    Start updating drumpad mini game to use sticks (still has bugs rn)

--- Assets/RhythmMiniGame/DrumRollScript.cs
+++ Assets/RhythmMiniGame/DrumRollScript.cs
@@ -4,42 +4,69 @@ using UnityEngine;
 
 public class DrumRollScript : MonoBehaviour
 {
-    private float upSpeed = 15f; // Adjust the movement speed as needed
-    private float downSpeed = 2.5f; // Adjust the movement speed as needed
-    private float maxY = -0.02f;      // Define your maximum Y position
-    private float minY = -4.14f;      // Define your minimum Y position
-    private float minGood = -3f;
-    private float maxGood = -1f;
+    public GameObject leftDrumStick;
+    public GameObject rightDrumStick;
+    private float inSpeed = 15f; 
+    private float outSpeed = 2.5f; 
+    private float maxRightX = 15.41f;
+    private float minRightX = 11.36f; // TBD!!!
+    private float minLeftX = -1.07f;
+    private float maxLeftX = 2.95f; // TBD!!!!
+    private float minGood = 13f;
+    private float maxGood = 1f;
     private SpriteRenderer sr;
+    bool toggleAlternateSticks = false;
+    private Vector3 leftStartPos;
+    private Vector3 rightStartPos;
 
         // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        leftStartPos = leftDrumStick.transform.position;
+        rightStartPos = rightDrumStick.transform.position;
+       // leftDrumStick.transform.localScale *= 2.0f;
+        rightDrumStick.transform.localScale *= 0.5f;
     }
     void Update()
     {
-        float newYPosition = transform.position.y;
+        float newLeftXPosition = leftDrumStick.transform.position.x;
+        float newRightXPosition = rightDrumStick.transform.position.x;
+
         // Check if the space bar is pressed
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // Calculate the new Y position
-            newYPosition = transform.position.y + upSpeed * Time.deltaTime;
+            newLeftXPosition += inSpeed * Time.deltaTime;
+            newRightXPosition -= inSpeed * Time.deltaTime;
+            if (toggleAlternateSticks)
+            {
+                leftDrumStick.transform.localScale *= 2.0f;
+                rightDrumStick.transform.localScale *= 0.5f;
+            } else
+            {
+                leftDrumStick.transform.localScale *= 0.5f;
+                rightDrumStick.transform.localScale *= 2.0f;
+            }
+            toggleAlternateSticks = !toggleAlternateSticks;
         } else
         {
-            newYPosition = transform.position.y - downSpeed * Time.deltaTime;
+            newLeftXPosition -= outSpeed * Time.deltaTime;
+            newRightXPosition += outSpeed * Time.deltaTime;
         }
-        // Clamp the Y position within the specified range
-        newYPosition = Mathf.Clamp(newYPosition, minY, maxY);
+        // Clamp the X position within the specified range
+        newLeftXPosition = Mathf.Clamp(newLeftXPosition, minLeftX, maxLeftX);
+        newRightXPosition = Mathf.Clamp(newRightXPosition, minRightX, maxRightX);
 
         // Update the game object's position
-        transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
+        rightDrumStick.transform.position = new Vector3(newRightXPosition, rightDrumStick.transform.position.y, rightDrumStick.transform.position.z);
+        leftDrumStick.transform.position = new Vector3(newLeftXPosition, leftDrumStick.transform.position.y, leftDrumStick.transform.position.z);
 
-        if (newYPosition > minGood && newYPosition < maxGood)
+        /*if (newYPosition > minGood && newYPosition < maxGood)
             sr.color = Color.green;
         else
-            sr.color = Color.red;
+            sr.color = Color.red;*/
     }
 
 
 }
+
