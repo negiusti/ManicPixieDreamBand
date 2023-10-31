37f14b6c8238c7d86a2bd1680967cd5bef8f700b
Nicole Giusti <nicoleegiusti@gmail.com>
2023-10-31 12:12:34 -0700

    Drumpad minigame: add shake

---
+++ Assets/DrumstickScript.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumstickScript : MonoBehaviour
+{
+    public bool isLeftStick;
+    private Vector3 startPos;
+    private Vector3 startScale;
+    private float inSpeed = 18f;
+    private float outSpeed = 2.5f;
+    private bool onDrumPad = false;
+
+    private float shakeDuration = 1.0f;
+    private float shakeMagnitude = 0.1f;
+    private float shakeSpeed = 30.0f;
+    private bool isShaking = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        startScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newXPosition = transform.position.x;
+        if (isShaking)
+            return;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float adjustment = inSpeed * Time.deltaTime;
+            if (isLeftStick)
+                newXPosition += adjustment;
+            else
+                newXPosition -= adjustment;
+        }
+        else if (onDrumPad)
+        {
+            float adjustment = outSpeed * Time.deltaTime;
+            if (isLeftStick)
+                newXPosition -= adjustment;
+            else
+                newXPosition += adjustment;
+        }
+        transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Drumpad")
+        {
+            onDrumPad = true;
+        }
+        if (collision.gameObject.name.Contains("drumstick"))
+        {
+            isShaking = true;
+            startScale = transform.localScale;
+            StartCoroutine(Shake());
+        }
+    }
+    public bool Shaking()
+    {
+        return isShaking;
+    }
+   
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Drumpad")
+        {
+            onDrumPad = false;
+        }
+    }
+
+    IEnumerator Shake()
+    {
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < shakeDuration)
+        {
+            Vector3 randomOffset = Random.insideUnitSphere * shakeMagnitude;
+            transform.position = transform.position + randomOffset;
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+        transform.position = startPos;
+        transform.localScale = startScale;
+        isShaking = false;
+    }
+}
--- Assets/RhythmMiniGame/DrumRollScript.cs
+++ Assets/RhythmMiniGame/DrumRollScript.cs
@@ -4,67 +4,47 @@ using UnityEngine;
 
 public class DrumRollScript : MonoBehaviour
 {
-    public GameObject leftDrumStick;
-    public GameObject rightDrumStick;
+    public DrumstickScript leftDrumStick;
+    public DrumstickScript rightDrumStick;
     private float inSpeed = 15f; 
     private float outSpeed = 2.5f; 
     private float maxRightX = 15.41f;
     private float minRightX = 11.36f; // TBD!!!
     private float minLeftX = -1.07f;
     private float maxLeftX = 2.95f; // TBD!!!!
-    private float minGood = 13f;
-    private float maxGood = 1f;
+    //private float minGood = 13f;
+    //private float maxGood = 1f;
     private SpriteRenderer sr;
     bool toggleAlternateSticks = false;
-    private Vector3 leftStartPos;
-    private Vector3 rightStartPos;
+    private Vector3 leftStartScale;
+    private Vector3 rightStartScale;
 
         // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-        leftStartPos = leftDrumStick.transform.position;
-        rightStartPos = rightDrumStick.transform.position;
-       // leftDrumStick.transform.localScale *= 2.0f;
-        rightDrumStick.transform.localScale *= 0.5f;
+        leftStartScale = leftDrumStick.transform.localScale;
+        rightStartScale = rightDrumStick.transform.localScale;
+        //rightDrumStick.transform.localScale *= 0.75f;
     }
     void Update()
     {
-        float newLeftXPosition = leftDrumStick.transform.position.x;
-        float newRightXPosition = rightDrumStick.transform.position.x;
-
+        if (leftDrumStick.Shaking() || rightDrumStick.Shaking())
+            return; 
         // Check if the space bar is pressed
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            newLeftXPosition += inSpeed * Time.deltaTime;
-            newRightXPosition -= inSpeed * Time.deltaTime;
             if (toggleAlternateSticks)
             {
-                leftDrumStick.transform.localScale *= 2.0f;
-                rightDrumStick.transform.localScale *= 0.5f;
+                leftDrumStick.transform.localScale = leftStartScale;
+                rightDrumStick.transform.localScale *= 0.75f;
             } else
             {
-                leftDrumStick.transform.localScale *= 0.5f;
-                rightDrumStick.transform.localScale *= 2.0f;
+                leftDrumStick.transform.localScale *= 0.75f;
+                rightDrumStick.transform.localScale = rightStartScale;
             }
             toggleAlternateSticks = !toggleAlternateSticks;
-        } else
-        {
-            newLeftXPosition -= outSpeed * Time.deltaTime;
-            newRightXPosition += outSpeed * Time.deltaTime;
         }
-        // Clamp the X position within the specified range
-        newLeftXPosition = Mathf.Clamp(newLeftXPosition, minLeftX, maxLeftX);
-        newRightXPosition = Mathf.Clamp(newRightXPosition, minRightX, maxRightX);
-
-        // Update the game object's position
-        rightDrumStick.transform.position = new Vector3(newRightXPosition, rightDrumStick.transform.position.y, rightDrumStick.transform.position.z);
-        leftDrumStick.transform.position = new Vector3(newLeftXPosition, leftDrumStick.transform.position.y, leftDrumStick.transform.position.z);
-
-        /*if (newYPosition > minGood && newYPosition < maxGood)
-            sr.color = Color.green;
-        else
-            sr.color = Color.red;*/
     }
 
 
