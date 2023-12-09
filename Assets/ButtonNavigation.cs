3009bfa78f9b63ed3c62b51e02d07b0eaac5f803
Nicole Giusti <nicoleegiusti@gmail.com>
2023-12-08 17:28:45 -0800

    Navigate menu response buttons with up/down arrow keys

---
+++ Assets/ButtonNavigation.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonNavigation : MonoBehaviour
+{
+    private Button[] selectables;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        // Get all Selectable components in the GameObject's children
+        selectables = GetComponentsInChildren<Button>();
+
+        // Ensure there is at least one selectable
+        if (selectables.Length > 0)
+        {
+            // Set the initial selection
+            EventSystem.current.SetSelectedGameObject(selectables[currentIndex].gameObject);
+            selectables[currentIndex].OnSelect(null);
+            selectables[currentIndex].Select();
+        }
+    }
+
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(selectables[currentIndex].gameObject);
+        selectables[currentIndex].OnSelect(null);
+        selectables[currentIndex].Select();
+    }
+
+    void Update()
+    {
+        // Check for up and down arrow key presses
+        //float verticalInput = Input.GetAxis("Vertical");
+
+        //if (verticalInput > 0)
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            // Move selection up
+            SelectNext(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            // Move selection down
+            SelectNext(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            selectables[currentIndex].OnSubmit(null);
+        }
+    }
+
+    void SelectNext(int direction)
+    {
+        // Deselect the current button
+        //selectables[currentIndex].OnDeselect(null);
+        selectables[currentIndex].OnDeselect(null);
+        
+        // Update the current index
+        currentIndex = (currentIndex + direction + selectables.Length) % selectables.Length;
+
+        // Select the new button
+        EventSystem.current.SetSelectedGameObject(selectables[currentIndex].gameObject);
+        selectables[currentIndex].OnSelect(null);
+        selectables[currentIndex].Select();
+    }
+}
