03d522d182361419f92856f05ad9c8c44f11c0db
Nicole Giusti <nicoleegiusti@gmail.com>
2023-12-18 17:43:15 -0800

    Makeupselector increases size when hovered

--- Assets/CharacterCreator/MakeupSelector.cs
+++ Assets/CharacterCreator/MakeupSelector.cs
@@ -6,11 +6,18 @@ public class MakeupSelector : MonoBehaviour
 {
     public string categoryName;
     public CharacterEditor characterEditor;
+    private Vector3 originalScale;
+    private SpriteRenderer spriteRenderer;
+    private MakeupSelector[] selectors;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = this.GetComponentInChildren<SpriteRenderer>();
+        originalScale = this.spriteRenderer.gameObject.transform.localScale;
+        selectors = FindObjectsOfType<MakeupSelector>();
+        //spriteRenderer.material.
     }
 
     // Update is called once per frame
@@ -22,5 +29,21 @@ public class MakeupSelector : MonoBehaviour
     private void OnMouseDown()
     {
         characterEditor.SetCurrentFaceCategory(categoryName);
+    }
+
+    private void OnMouseEnter()
+    {
+        Vector3 newScale = originalScale + Vector3.one * 0.2f;
+        this.spriteRenderer.gameObject.transform.localScale = newScale;
+    }
+
+    private void OnMouseExit()
+    {
+        spriteRenderer.gameObject.transform.localScale = originalScale;
+    }
+
+    public void UnSelect()
+    {
+
     }
 }
