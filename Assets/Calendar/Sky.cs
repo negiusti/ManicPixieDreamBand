0f8cc20ef6e98109b3c22139823ec15dfd287edb
Nicole Giusti <nicoleegiusti@gmail.com>
2024-04-28 19:31:31 -0700

    Weather system [WIP]

--- Assets/Calendar/Sky.cs
+++ Assets/Calendar/Sky.cs
@@ -4,14 +4,26 @@ using UnityEngine.U2D.Animation;
 public class Sky : MonoBehaviour
 {
     private SpriteResolver spriteResolver;
+    private ParticleSystem particles;
     // Start is called before the first frame update
     void Start()
     {
         spriteResolver = this.GetComponent<SpriteResolver>();
+        particles = this.GetComponentInChildren<ParticleSystem>();
     }
 
     public void UpdateSky()
     {
+        Weather.WeatherState currentWeather = Weather.Current();
+        switch (currentWeather)
+        {
+            case Weather.WeatherState.Rainy:
+                particles.gameObject.SetActive(true);
+                break;
+            default:
+                particles.gameObject.SetActive(false);
+                break;
+        }
         string label = Calendar.IsDay() ? "Day" : "Night";
         if (spriteResolver == null)
             Start();
---
+++ Assets/Calendar/Weather.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weather", menuName = "Custom/Weather")]
+public class Weather : ScriptableObject
+{
+   public enum WeatherState
+    {
+        Cloudy,
+        Rainy,
+        Clear
+    }
+
+    private static WeatherState currentWeather;
+
+    public static WeatherState Current()
+    {
+        return currentWeather;
+    }
+
+}
