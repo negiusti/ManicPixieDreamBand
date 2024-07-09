cc3672b66f6675a00976d717e69603a8cc4c7185
Nicole Giusti <nicoleegiusti@gmail.com>
2024-07-08 17:03:12 -0700

    Fix bug with pockets app showing incorrect icons

--- Assets/Phone/PocketsAppItem.cs
+++ Assets/Phone/PocketsAppItem.cs
@@ -4,29 +4,23 @@ using UnityEngine.UI;
 
 public class PocketsAppItem : MonoBehaviour
 {
-    private SpriteResolver itemIcon;
+    public SpriteResolver itemIcon;
     private SpriteResolver slotIcon;
-    private SpriteRenderer itemRen;
+    public SpriteRenderer itemRen;
     private SpriteRenderer slotRen;
     private Image slotImg;
-    private Image itemImg;
+    public Image itemImg;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        itemIcon = GetComponentInChildren<SpriteResolver>(includeInactive: true);
-        itemImg = GetComponentInChildren<Image>(includeInactive: true);
-        itemRen = GetComponentInChildren<SpriteRenderer>(includeInactive: true);
         slotIcon = GetComponent<SpriteResolver>();
         slotImg = GetComponent<Image>();
         slotRen = GetComponent<SpriteRenderer>();
         // randomize slot icon
         slotIcon.SetCategoryAndLabel("Slot", Random.Range(0, 3).ToString());
         slotIcon.ResolveSpriteToSpriteRenderer();
-        //itemRen.enabled = true;
-        //itemImg.sprite = itemRen.sprite;
-        //itemRen.enabled = false;
         slotImg.sprite = slotRen.sprite;
     }
 
@@ -38,16 +32,11 @@ public class PocketsAppItem : MonoBehaviour
 
     public void SetItemIcon(string itemName)
     {
-        Debug.Log("SetItemIcon: " + itemName);
         if (itemRen == null)
             Start();
         itemIcon.SetCategoryAndLabel("Item", itemName);
-        itemRen.enabled = true;
         itemIcon.ResolveSpriteToSpriteRenderer();
         itemImg.sprite = itemRen.sprite;
-        itemRen.enabled = false;
-        Debug.Log("SetItemIcon: " + itemRen.sprite.name);
-        Debug.Log("SetItemIcon: " + itemImg.sprite.name);
     }
 
     private void OnEnable()
