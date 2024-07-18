c10ab8419e8fdbf7be217c3e603be0ddbeca6a04
Nicole Giusti <nicoleegiusti@gmail.com>
2024-07-18 13:54:10 -0700

    darken caboodle sections when hovered (instead of scaling size)

--- Assets/CharacterCreator/CaboodleSection.cs
+++ Assets/CharacterCreator/CaboodleSection.cs
@@ -3,17 +3,29 @@ using UnityEngine;
 public class CaboodleSection : MonoBehaviour
 {
     private Animator animator;
+    private Material mat;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        mat = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseEnter()
+    {
+        mat.SetColor("_Color", new Color(.7f, .7f, .7f));
+    }
+
+    private void OnMouseExit()
+    {
+        mat.SetColor("_Color", Color.white);
     }
 
     public void Select()
