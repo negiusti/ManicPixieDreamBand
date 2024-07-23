36f22c4c0dfabdb0e247058ee020d1ca8e30e9c7
Nicole Giusti <nicoleegiusti@gmail.com>
2024-07-23 13:32:05 -0700

    skin tone icon WIP

--- Assets/CharacterCreator/ColorPalette.cs
+++ Assets/CharacterCreator/ColorPalette.cs
@@ -1,20 +1,33 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ColorPalette : MonoBehaviour
 {
     private CharacterEditor characterEditor;
     public string category;
-
+    private bool showSkinTones;
+    private bool coroutineDone;
     // Start is called before the first frame update
     void Start()
     {
         characterEditor = GameObject.FindObjectOfType<CharacterEditor>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+       coroutineDone = true;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
     }
 
     public void SetColor(Color c)
@@ -25,4 +38,52 @@ public class ColorPalette : MonoBehaviour
             characterEditor.SetCurrentFaceCategoryColor(c);
     }
 
+    private void OnMouseDown()
+    {
+        Debug.Log("coroutineDone: "+ coroutineDone);
+        if (!coroutineDone)
+            return;
+        if (!category.Equals("Skin"))
+            return;
+        coroutineDone = false;
+        showSkinTones = !showSkinTones;
+        if (showSkinTones)
+            StartCoroutine(ShowSkinTones());
+        else
+            StartCoroutine(HideSkinTones());
+    }
+
+    private IEnumerator ShowSkinTones()
+    {
+        for (int i = 0; i < transform.childCount; i++) {
+            transform.GetChild(i).gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.05f);
+        }
+        coroutineDone = true;
+    }
+
+    private IEnumerator HideSkinTones()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.05f);
+        }
+        coroutineDone = true;
+    }
+
+    //public void OnPointerDown(PointerEventData eventData)
+    //{
+    //    Debug.Log("coroutineDone: " + coroutineDone);
+    //    if (!coroutineDone)
+    //        return;
+    //    if (!category.Equals("Skin"))
+    //        return;
+    //    coroutineDone = false;
+    //    showSkinTones = !showSkinTones;
+    //    if (showSkinTones)
+    //        ShowSkinTones();
+    //    else
+    //        HideSkinTones();
+    //}
 }
---
+++ Assets/SleepingScreenMiniGame.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class SleepingScreenMiniGame : MiniGame
+{
+    private GameObject mainCamera;
+    private GameObject mgCamera;
+    private bool isActive;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        mgCamera = GetComponentInChildren<Camera>().gameObject;
+        DisableAllChildren();
+        //OpenMiniGame();
+    }
+
+    public override bool IsMiniGameActive()
+    {
+        return isActive;
+    }
+
+    public override void OpenMiniGame()
+    {
+        // Opening up the minigame
+
+        mainCamera = Camera.main.transform.gameObject;
+
+        mainCamera.SetActive(false);
+        mainCamera.transform.position = new Vector3(transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        mgCamera.transform.position = new Vector3(transform.position.x, mgCamera.transform.position.y, mgCamera.transform.position.z);
+        EnableAllChildren();
+
+        MiniGameManager.PrepMiniGame();
+        isActive = true;
+        animator.Play("SleepingLoadingScreen_Anim", -1, 0f);
+        Calendar.Sleep();
+        Characters.MainCharacter().transform.position = new Vector3(transform.position.x, Characters.MainCharacter().transform.position.y, Characters.MainCharacter().transform.position.z);
+    }
+
+    public override void CloseMiniGame()
+    {
+        mainCamera.transform.position = new Vector3(mgCamera.transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        if (mainCamera != null)
+            mainCamera.SetActive(true);
+
+        DisableAllChildren();
+
+        isActive = false;
+        MiniGameManager.CleanUpMiniGame();
+    }
+
+}
