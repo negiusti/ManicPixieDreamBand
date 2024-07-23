fac03776374374cf2152fd63792655d00ed5e7c4
Nicole Giusti <nicoleegiusti@gmail.com>
2024-07-23 09:48:00 -0700

    Play sleeping screen when sleeping!

--- Assets/Bed.cs
+++ Assets/Bed.cs
@@ -1,16 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bed : MonoBehaviour
 {
     private Animator animator;
+    public Animator sleepingScreen;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         animator.SetBool("DoneForTheDay", Calendar.DoneForTheDay());
         GetComponent<BiggerWhenHovered>().scaleFactor = Calendar.DoneForTheDay() ? 1.1f : 1f;
+        sleepingScreen.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,9 +28,11 @@ public class Bed : MonoBehaviour
     {
         if (Calendar.DoneForTheDay())
         {
-            animator.Play("Sleep", -1, 0f);
+            sleepingScreen.gameObject.SetActive(true);
+            sleepingScreen.Play("SleepingLoadingScreen_Anim", -1, 0f);
             animator.SetBool("DoneForTheDay", false);
             GetComponent<BiggerWhenHovered>().scaleFactor = 1f;
+            Calendar.Sleep();
         }
     }
 }
