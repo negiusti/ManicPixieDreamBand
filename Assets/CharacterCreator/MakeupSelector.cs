d0fc99f022b8a2a692d5d56ff4d908baf71237ce
Nicole Giusti <nicoleegiusti@gmail.com>
2024-07-30 00:02:38 -0700

    disable old caboodle icon buttons

--- Assets/CharacterCreator/MakeupSelector.cs
+++ Assets/CharacterCreator/MakeupSelector.cs
@@ -1,4 +1,3 @@
-using TMPro;
 using UnityEngine;
 
 public class MakeupSelector : MonoBehaviour
@@ -6,26 +5,12 @@ public class MakeupSelector : MonoBehaviour
     public string categoryName;
     private CharacterEditor characterEditor;
     private Vector3 originalScale;
-    private SpriteRenderer spriteRenderer;
-    private MakeupSelector[] selectors;
-    public Material defaultMat;
-    public Material outlineMat;
-    private Animator anim;
-    public string hintText;
-    private TextMeshPro tmp;
+
     // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer = this.GetComponentInChildren<SpriteRenderer>();
-        originalScale = this.spriteRenderer.gameObject.transform.localScale;
-        selectors = this.transform.parent.gameObject.GetComponentsInChildren<MakeupSelector>();
+        originalScale = transform.localScale;
         characterEditor = GameObject.FindObjectOfType<CharacterEditor>();
-        spriteRenderer.material = defaultMat;
-        anim = this.GetComponent<Animator>();
-        anim.enabled = false;
-        tmp = this.GetComponentInChildren<TextMeshPro>();
-        tmp.text = hintText;
-        tmp.enabled = false;
     }
 
     // Update is called once per frame
@@ -41,40 +26,16 @@ public class MakeupSelector : MonoBehaviour
     private void OnMouseEnter()
     {
         Vector3 newScale = originalScale * 1.1f;
-        this.spriteRenderer.gameObject.transform.localScale = newScale;
-        //anim.CrossFade("MakeupMenu_Anim", 0f, 0);
-        //anim.Play("MakeupMenu_Anim", 0, 0f);
-        tmp.enabled = true;
+        transform.localScale = newScale;
     }
 
     private void OnMouseExit()
     {
-        spriteRenderer.gameObject.transform.localScale = originalScale;
-        tmp.enabled = false;
-    }
-
-    public void UnSelect()
-    {
-        spriteRenderer.material = defaultMat;
-        anim.enabled = false;
-    }
-
-    private void UnSelectAll()
-    {
-        foreach (MakeupSelector selector in selectors)
-        {
-            selector.UnSelect();
-        }
+        transform.localScale = originalScale;
     }
 
     public void Select()
     {
-        //anim.ResetTrigger("MakeupSelectorTrigger");
-        //anim.SetTrigger("MakeupSelectorTrigger");
-        UnSelectAll();
-        anim.enabled = true;
-        anim.Play("MakeupMenu_Anim", 0, 0f);
         characterEditor.SetCurrentFaceCategory(categoryName);
-        spriteRenderer.material = outlineMat;
     }
 }
