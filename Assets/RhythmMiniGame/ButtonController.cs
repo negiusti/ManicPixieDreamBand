89ea2f26f83be45705524360a868c5725b0a0a3b
Nicole Giusti <nicoleegiusti@gmail.com>
2024-08-05 13:23:12 -0700

    change bass mg button colors

--- Assets/RhythmMiniGame/ButtonController.cs
+++ Assets/RhythmMiniGame/ButtonController.cs
@@ -10,6 +10,8 @@ public class ButtonController : MonoBehaviour
     public Sprite pressedSprite;
     public SpriteResolver guitarString;
     public KeyCode keyCode;
+    private Color pressedColor = new Color(219/255f, 101/255f, 156/255f);
+    private Color unpressedColor = new Color(1f, 197/255f, 220/255f);
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +26,13 @@ public class ButtonController : MonoBehaviour
         if (Input.GetKeyDown(keyCode))
         {
             spriteRenderer.sprite = pressedSprite;
-            spriteRenderer.color = Color.green;
+            spriteRenderer.color = pressedColor;
             guitarString.SetCategoryAndLabel("String", "Wiggly");
         }
         if (Input.GetKeyUp(keyCode))
         {
             spriteRenderer.sprite = defaultSprite;
-            spriteRenderer.color = Color.cyan;
+            spriteRenderer.color = unpressedColor;
             guitarString.SetCategoryAndLabel("String", "Still");
         }
     }
