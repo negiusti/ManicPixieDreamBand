383ff4aebae00e40565a0c78a7ae7d835c797230
Nicole Giusti <nicoleegiusti@gmail.com>
2024-08-05 14:30:03 -0700

    remove button prompts from character editor dialogue bubbles

--- Assets/RhythmGameHint.cs
+++ Assets/RhythmGameHint.cs
@@ -7,19 +7,21 @@ public class RhythmGameHint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SetChildrenActive(!Tutorial.playedBass);
+        //SetChildrenActive(!Tutorial.playedBass);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Alpha4))
+        //    gameObject.SetActive(false);
     }
 
-    private void SetChildrenActive(bool active)
-    {
-        foreach (Transform child in transform)
-        {
-            child.gameObject.SetActive(active);
-        }
-    }
+    //private void SetChildrenActive(bool active)
+    //{
+    //    foreach (Transform child in transform)
+    //    {
+    //        child.gameObject.SetActive(active);
+    //    }
+    //}
 }
