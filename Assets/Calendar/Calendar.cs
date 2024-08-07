feca2444b14c9275f83e86bdca774d65d0353837
Nicole Giusti <nicoleegiusti@gmail.com>
2024-08-07 14:27:18 -0700

    add pixie's photo

--- Assets/Calendar/Calendar.cs
+++ Assets/Calendar/Calendar.cs
@@ -55,7 +55,7 @@ public class Calendar : ScriptableObject
             events.Add(day + (int)daysFromNow, new List<ICalendarEvent>());
         if (!events[day + (int)daysFromNow].Any(e => e.Name().Equals(eventName)))  // Don't schedule duplicate events!
         {
-            events[day + (int)daysFromNow].Add(new QuestEvent(eventName, conversation, isNight, location));
+            events[day + (int)daysFromNow].Insert(0, new QuestEvent(eventName, conversation, isNight, location));
             // TODO: trigger calendar app notif
             if (daysFromNow == 0) // New event was added today!
                 Phone.Instance.SendNotificationTo("Calendar");
