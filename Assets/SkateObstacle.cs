1abe95e84adf70e4d23baeb2ecc0c64b3ce68827
Nicole Giusti <nicoleegiusti@gmail.com>
2024-09-05 17:07:18 -0700

    Randomize x position of skate obstacles

---
+++ Assets/SkateObstacle.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class SkateObstacle : MonoBehaviour
+{
+    public GameObject background; // Reference to the background GameObject
+    private Bounds backgroundBounds;
+    private float minX, maxX; // Minimum and maximum x positions for the obstacle
+
+    void Start()
+    {
+        if (background != null)
+        {
+            // Calculate the camera bounds based on the background's collider
+            backgroundBounds = background.GetComponent<Collider2D>().bounds;
+            minX = backgroundBounds.min.x + 5;
+            maxX = backgroundBounds.max.x - 5;
+            transform.position = new Vector3(Random.Range(minX, maxX), transform.position.y, transform.position.z);
+        }
+        else
+        {
+            Debug.LogError("Background GameObject not assigned.");
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (backgroundBounds == null)
+            return;
+
+        // Random transform x position
+        transform.position = new Vector3(Random.Range(minX, maxX), transform.position.y, transform.position.z);
+    }
+}
