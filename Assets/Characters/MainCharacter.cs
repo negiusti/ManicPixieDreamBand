61a00744b8f6e66ab84564dc5e537987584ad268
Nicole Giusti <nicoleegiusti@gmail.com>
2025-04-10 11:35:34 -0700

    fix remap bug with proximity selector

--- Assets/Characters/MainCharacter.cs
+++ Assets/Characters/MainCharacter.cs
@@ -1,15 +1,19 @@
 using PixelCrushers.DialogueSystem;
 using UnityEngine;
+using Rewired;
 
 
 public class MainCharacter : MonoBehaviour
 {
     private ProximitySelector ps;
+    private Player player;
+
 
     // Start is called before the first frame update
     void Start()
     {
         ps = this.GetComponent<ProximitySelector>();
+        player = ReInput.players.GetPlayer(0);
     }
 
     // Update is called once per frame
@@ -21,6 +25,10 @@ public class MainCharacter : MonoBehaviour
         } else if (!DialogueManager.isConversationActive && !ps.enabled)
         {
             ps.enabled = true;
+        }
+        if (!DialogueManager.isConversationActive && ps.enabled && player.GetButtonDown("Interact"))
+        {
+            ps.UseCurrentSelection();
         }
     }
 }
