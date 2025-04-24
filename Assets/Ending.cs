3aab0723656628ecddf70cfedecba83307536449
Nicole Giusti <nicoleegiusti@gmail.com>
2025-04-24 15:03:59 -0700

    ending scene

---
+++ Assets/Ending.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Ending : MonoBehaviour
+{
+    private Animator animator;
+    private bool startedEndingAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();    
+        startedEndingAnim = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startedEndingAnim && !SceneChanger.Instance.IsLoadingScreenOpen()) {
+            startedEndingAnim = true;
+            animator.Play("Ending", -1, 0f);
+        }
+    }
+
+    public void Done()
+    {
+        SceneChanger.Instance.ChangeScene("Splash");
+    }
+}
